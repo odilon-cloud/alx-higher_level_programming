abafaeecc25476231f8f032799e3dc7470d6a139
odilon-cloud <oirambna@gmail.com>
2022-06-03 23:40:12 +0000

    fix pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ while n <= 89:
     if n % 10 == 0:
         n += 1 + n // 10
     print("{:02d}".format(n), end='\n' if n == 89 else ", ")
-    n+= 1
+    n += 1
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 
 
 def print_last_digit(number):
-    last_num= (number % 10) if number >= 0 else ((number * -1) % 10)
+    last_num = (number % 10) if number >= 0 else ((number * -1) % 10)
     print(last_num, end='')
     return (last_num)
