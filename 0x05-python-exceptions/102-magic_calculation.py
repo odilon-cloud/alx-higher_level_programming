a224cd53d79a756d3c6388a4bef634d3883cbadf
odilon-cloud <oirambna@gmail.com>
2022-06-21 17:56:54 +0000

    commit

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too Far')
             else:
                 result += (a ** b) / i
-        except TypeError:
+        except :
             result = b + a
             break
     return (result)
