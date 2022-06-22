265b61389adaa23d791d9dc8e3390ae12c479569
odilon-cloud <oirambna@gmail.com>
2022-06-22 01:28:01 +0000

    commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Summary line for a class docstring should fit on one line."""
+        return (self.__size * self.__size)
+
