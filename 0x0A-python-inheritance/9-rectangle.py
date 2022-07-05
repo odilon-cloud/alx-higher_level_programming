806aac27ae5b97b10ede93dee1173fa2f129d752
odilon-cloud <oirambna@gmail.com>
2022-07-05 21:29:07 +0000

    fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""BaseGeometry class module"""
+
+
+class BaseGeometry:
+    """Geomerty utility class"""
+
+    def area(self):
+        """Returns the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
+    """Rectangle geometry class"""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+        """Sets the width and height of a Rectangle instance
+            width (int): The width of the Rectangle
+            height (int): The height of the Rectange
         """
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Return the area of the rectangle."""
+        """``area`` method implemenation for Rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        """Returns a string description of the Rectangle"""
+        return "[{}] {}/{}".format(
+                self.__class__.__name__, self.__width, self.__height)
