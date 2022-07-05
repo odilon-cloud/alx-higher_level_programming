3eb54ca1f07f14e5004e1cd2b176e0698df7288a
odilon-cloud <oirambna@gmail.com>
2022-07-05 21:25:39 +0000

    commit fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,3 @@ def is_kind_of_class(obj, a_class):
     """Returns True if the object is an instance of class, inherited"""
 
     return isinstance(obj, a_class)
-
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""Represent BaseGeometry class"""
+"""BaseGeometry class module"""
 
 
 class BaseGeometry:
-    """Reprsent base geometry."""
+    """Geomerty utility class"""
 
     def area(self):
-        """Not yet implemented."""
+        """Represent area
+        Raises:
+            Exception: [area() is not implemented]
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
+        """Validates value
+        Arguments:
+            name {str} -- name of an instance
+            value {int} -- type of instance
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
+            TypeError: [must be an integer]
+            ValueError: [must be greater than 0]
         """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
