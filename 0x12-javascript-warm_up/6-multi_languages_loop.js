72acaeb7d8230cbc7942c913a2ea8d80c363703a
odilon-cloud <oirambna@gmail.com>
2022-09-05 22:19:28 +0000

    commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const val of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(val);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
