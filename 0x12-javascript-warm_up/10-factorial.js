7652602aaaf551d98d3232c5296f8966128a061d
odilon-cloud <oirambna@gmail.com>
2022-09-05 21:10:44 +0000

    commit

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+'use strict';
+
+function factorial (a) {
+  if (a <= 1) {
+    return (1);
+  }
+  return (a * factorial(a - 1));
+}
+
+let a = Math.floor(process.argv[2]);
+if (isNaN(a)) {
+  console.log(1);
+} else {
+  console.log(factorial(a));
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+'use strict';
+const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let s of line) {
+  console.log(s);
+}
