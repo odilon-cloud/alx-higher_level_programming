83c43863d0e25f40624fed9338eaeb090fc69355
odilon-cloud <oirambna@gmail.com>
2022-09-05 22:17:32 +0000

    added task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-'use strict';
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+for (const val of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(val);
 }
